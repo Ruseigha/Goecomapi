9c328a0491e221239e46140a707bdced7b6e7228
Seigha Rufus <excellenceseigha@gmail.com>
2026-01-19 15:40:00 +0100

    feat:create user domain model

---
+++ internal/domain/user.go
@@ -0,0 +1,20 @@
+package domain
+
+import "time"
+
+type Role string
+
+const (
+	RoleUser  Role = "user"
+	RoleAdmin Role = "admin"
+)
+
+type User struct {
+	ID           string    `bson:"_id,omitempty" json:"id,omitempty"`
+	Name         string    `bson:"name" json:"name"`
+	Email        string    `bson:"email" json:"email"`
+	PasswordHash string    `bson:"password_hash" json:"-"`
+	Role         Role      `bson:"role" json:"role"`
+	CreatedAt    time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt    time.Time `bson:"updated_at" json:"updated_at"`
+}
